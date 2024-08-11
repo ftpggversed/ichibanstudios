a1d519658e200e3f8e9a7b0797ee3a098cdd483d
NexusSZN <damianbennett2703@gmail.com>
2024-08-10 21:01:18 -0400

    On branch main Your branch is up to date with 'ichibanstudios/main'.
    Changes to be committed:
    	modified:   admin.html
    	modified:   css/admin.css
    	new file:   css/dashboard.css
    	modified:   dashboard.html
    	modified:   js/admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,15 @@
 document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    var password = document.getElementById("password").value;
-  
-    // Replace "admin" with your desired password
-    if (password === "admin") {
-      window.location.href = "dashboard.html";
-    } else {
-      alert("Incorrect password. Please try again.");
-    }
-  });
+  event.preventDefault();
+
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Replace "admin" with your desired username
+  // Replace "admin" with your desired password
+  if (username === "admin" && password === "admin") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Incorrect username or password. Please try again.");
+  }
+});
+
